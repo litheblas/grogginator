ab3646847ebbb71473d966eb7e864eae58e1061a
William Torberntsson <william.torberntsson@gmail.com>
2022-05-28 21:15:56 +0200

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  document.title = "Grogginator";
+  
+  const size = [4, 4, 4, 4, 6, 6, 6, 6, 8, 8, 10, 12];
+  const color = ["röd", "gul", "grön", "blå", "svart", "rosa", "färglös"];
+  const taste = ["god", "fruktig", "mjuk", "besk", "söt", "äcklig"];
+  const adjective = ["skön", "trevlig", "busig", "spännande", "dansk", "heterogen", "homogen"];
+  const thing = ["golvfest", "krock", "stövel", "emulsion", "käftsmäll", "dansk", "fegis"];
+  const skit = ["god", "äcklig", "nasty", "vatten", "trevlig", "juice"];
+  const text = ["våga dö", "inget tjaffs", "fisk", "billig"];
+
+  const functions = [colorTaste, thingAdjective, skitDrink, testDrink, nobodyRemeber];
+
+  const [drinkOrder, setDrinkOrder] = useState("");
+
+  function getOrder(...array) {
+    let order = "";
+    array.forEach(array => {
+      if (typeof array === 'string') {
+        order += array;
+      } else {
+        order += array[Math.floor(Math.random() * array.length)];
+      }
+    })
+    return order;
+  }
+
+  function callModel() {
+    const model = functions[Math.floor(Math.random() * functions.length)];
+    model();
+  }
+
+  function colorTaste() {
+    const drink = getOrder("En ", size, " ", color, " och ", taste);
+    setDrinkOrder(drink + "!");
+  }
+
+  function thingAdjective() {
+    const drink = getOrder("En ", size, " ", adjective, " ", thing);
+    setDrinkOrder(drink + "!");
+  }
+
+  function skitDrink() {
+    const drink = getOrder("En ", size, " skit", skit);
+    setDrinkOrder(drink + "!");
+  }
+
+  function testDrink() {
+    const drink = getOrder("En ", size," ", text);
+    setDrinkOrder(drink + "!");
+  }
+
+  function nobodyRemeber() {
+    const drink = getOrder("En ", size, " ingen minns en ", thing);
+    setDrinkOrder(drink + "!");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="top">
+        {drinkOrder}
+      </div>
+      <div className="bottom">
+        <button onClick={() => callModel()}>Generera drink!</button>
+      </div>
     </div>
   );
 }
