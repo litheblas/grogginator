d17b598dccd1b3a9b1ae9d72244b0db1c940d4b5
William Torberntsson <william.torberntsson@gmail.com>
2022-06-09 11:02:17 +0200

    tweaked probabilited and lowered wait time

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import CountDown from 'react-countdown';
 export default function GenerateDrink() {
   
   let sizeprob = 0;
-  const wantedDelay = 180000; // 3 min
+  const wantedDelay = 60 * 1000; // 1 min
   const [drinkOrder, setDrinkOrder] = useState("");
   const [probability, setProbability] = useState(0);
   const [rarity, setRarity] = useState("");
@@ -20,10 +20,8 @@ export default function GenerateDrink() {
 
   // Check if user reloaded page and restore timer
   useEffect(() => {
-    console.log("useEffect")
     const futureDate = getLocalStorageValue("end_date");
     if (futureDate != null && !isNaN(futureDate)) {
-      console.log("time exists")
       const currentTime = Date.now();
       const delta = parseInt(futureDate, 10) - currentTime;
       //Do you reach the end?
@@ -33,7 +31,6 @@ export default function GenerateDrink() {
           localStorage.removeItem("end_date");
       } else {
         //No update the end date  
-        console.log("Update time")
         setDrinkOrder("Du kan inte spamma fram drinkar!")
         setRarity("Fuskare")
         setRarityStyle("Common")
@@ -109,75 +106,76 @@ export default function GenerateDrink() {
   const callModel = () => {
     const randnr = Math.random();
     let prob = 0;
+    console.log("random nr: ",randnr)
 
     // Calls corresponding function for random number and calculates probability
-    if (0 <= randnr && randnr < 0.30) { // colorTaste
+    if (0 <= randnr && randnr < 0.25) { // colorTaste
       functions[0]();
-      prob = 0.30 * 1 / (cons.color.length * cons.taste.length);
+      prob = 0.25 * 1 / (cons.color.length * cons.taste.length);
       setRarity("Everywhere")
       setRarityStyle("Everywhere")
 
-    } else if (0.30 <= randnr && randnr < 0.55) { // thingAdjective
+    } else if (0.25 <= randnr && randnr < 0.47) { // thingAdjective
       functions[1]();
-      prob = 0.25 * 1 / (cons.adjective.length * cons.thing.length);
+      prob = 0.22 * 1 / (cons.adjective.length * cons.thing.length);
       setRarity("Common")
       setRarityStyle("Common")
 
-    } else if (0.55 <= randnr && randnr < 0.8) { // skitDrink
+    } else if (0.47 <= randnr && randnr < 0.67) { // skitDrink
       functions[2]();
-      prob = 0.25 * 1 / (cons.thing.length);
+      prob = 0.20 * 1 / (cons.thing.length);
       setRarity("Uncommon")
       setRarityStyle("Uncommon")
 
-    } else if (0.8 <= randnr && randnr < 0.9) { // textDrink
+    } else if (0.67 <= randnr && randnr < 0.82) { // textDrink
       functions[3]();
-      prob = 0.1 * 1 / (cons.skit.length);
+      prob = 0.15 * 1 / (cons.skit.length);
       setRarity("Very Uncommon")
       setRarityStyle("VeryUncommon")
 
-    } else if (0.9 <= randnr && randnr < 0.96) { // nobodyRemeber
+    } else if (0.82 <= randnr && randnr < 0.92) { // nobodyRemeber
       functions[4]();
-      prob = 0.06 * 1 / (cons.thing.length);
+      prob = 0.10 * 1 / (cons.thing.length);
       setRarity("Rare")
       setRarityStyle("Rare")
 
-    } else if (0.96 <= randnr && randnr < 0.99) { // oneThing
+    } else if (0.92 <= randnr && randnr < 0.98) { // oneThing
       functions[5]();
-      prob = 0.03 * 1 / (cons.text.length);
+      prob = 0.06 * 1 / (cons.text.length);
       setRarity("Epic")
       setRarityStyle("Epic epicAnim")
     
-    } else if (0.99 <= randnr && randnr <= 1) {
+    } else if (0.98 <= randnr && randnr <= 1) {
       functions[6]();
-      prob = 0.01 * 1 / cons.vågadösize.length;
+      prob = 0.02 * 1 / cons.vågadösize.length;
       setRarity("LEGENDARY")
       setRarityStyle("Legendary legendAnim")
     
     } else {
       functions[0]();
-      prob = 0.35 * 1 / (cons.color.length * cons.taste.length);
+      prob = 0.25 * 1 / (cons.color.length * cons.taste.length);
       setRarity("Common")
       setRarityStyle("Common")
       console.log("Should not happen, colorTaste")
     }
     prob = prob * sizeprob * 100;
-    setProbability(prob.toFixed(3)); // set percentage decimals
+    setProbability(prob.toFixed(4)); // set percentage decimals
     return (prob)
   }
 
-  // 0 -> 30%   (30%)
+  // 0 -> 25%   (25%)
   const colorTaste = () => setDrinkOrder(getOrder(cons.color, " och ", cons.taste, "!"));
-  // 30 -> 55%  (25%)
+  // 25 -> 47%  (22%)
   const thingAdjective = () => setDrinkOrder(getOrder(cons.adjective, " ", cons.thing, "!"));
-  // 55 -> 80% (25%)
+  // 47 -> 67% (20%)
   const oneThing = () => setDrinkOrder(getOrder(cons.thing, "!"));
-  // 80 -> 90%  (10%)
+  // 67 -> 82%  (15%)
   const skitDrink = () => setDrinkOrder(getOrder("skit", cons.skit, "!"));
-  // 90 -> 96%  (6%)
-  const nobodyRemeber = () => setDrinkOrder(getOrder("ingen minns en ", cons.thing, "!"));
-  // 96 -> 99%  (3%)
+  // 82 -> 92%  (10%)
+  const nobodyRemeber = () => setDrinkOrder(getOrder("ingen minns en: ", cons.thing, "!"));
+  // 92 -> 98%  (6%)
   const textDrink = () => setDrinkOrder(getOrder(cons.text, "!"));
-  // 99 -> 100% (1%)
+  // 98 -> 100% (2%)
   const vågaDö = () => setDrinkOrder(vågaDögetOrder("våga dö!"))
 
 
@@ -199,7 +197,6 @@ export default function GenerateDrink() {
         key={date.date}
         onStart={() => {
           //Save the end date
-          console.log("onStart: ");
           if (localStorage.getItem("end_date") == null)
             localStorage.setItem(
               "end_date",
